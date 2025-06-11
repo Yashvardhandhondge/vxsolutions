9c29bc5918f4bc7cc842a99d98dd4cb0abb3efd2
Srikanth Katta <ikan@Srikanths-MacBook-Pro-2.local>
2025-06-11 22:50:02 +0530

    Disable next optimization for images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@ const repoName = 'vx-website';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
